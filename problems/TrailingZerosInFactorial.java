7632eccccf0d562aa7f243b7746c1345dbd4f0ad
reubenkl <reubenkl1602@gmail.com>
2024-03-14 02:50:26 +0530

    added efficient solution to trailing zeros in factorial problem

--- problems/TrailingZerosInFactorial.java
+++ problems/TrailingZerosInFactorial.java
@@ -5,8 +5,16 @@ import java.math.BigInteger;
 public class TrailingZerosInFactorial {
     public static void main(String[] args) {
         Integer input = 100;
-        Integer result = trailingZerosCountInFactorial(input);
+        Integer result = trailingZerosCountInFactorialEfficientSolution(input);
         System.out.println(input+"! contains "+result+" trailing Zeros");
+    }
+
+    private static Integer trailingZerosCountInFactorialEfficientSolution(Integer input) {
+        Integer result = 0;
+        for(int i=5; i<=input; i=i*5){
+            result = result+ input/i;
+        }
+        return result;
     }
 
     private static Integer trailingZerosCountInFactorial(Integer input) {
@@ -21,7 +29,6 @@ public class TrailingZerosInFactorial {
             result++;
             fact = fact.divide(BigInteger.TEN);
         }
-
         return result;
     }
 }
